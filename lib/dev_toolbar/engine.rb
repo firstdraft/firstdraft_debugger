11e3ad90ac44f59b7928980a16506954e1c5dc61
Murugan <murugu2012@gmail.com>
2017-10-25 13:29:18 +0530

    Extract middleware to separate gem

--- lib/dev_toolbar/engine.rb
+++ lib/dev_toolbar/engine.rb
@@ -2,12 +2,6 @@ module DevToolbar
   class Engine < ::Rails::Engine
     isolate_namespace DevToolbar
 
-    initializer "dev_toolbar.configure_rails_initialization" do |app|
-      if enable?
-        insert_middleware
-      end
-    end
-
     initializer "dev_toolbar.assets" do |app|
       Rails.application.config.assets.precompile += %w{ dev_toolbar }
       Rails.application.config.assets.paths << root.join("app", "assets", "stylesheets", "dev_toolbar")
@@ -19,31 +13,5 @@ module DevToolbar
       end
     end
 
-    config.after_initialize do
-      path = Rails.root.join('whitelist.yml')
-      if File.exist?(path)
-        whitelisted_ips = YAML.load_file(path)
-        whitelisted_ips.each do |ip|
-          BetterErrors::Middleware.allow_ip!(ip)
-        end
-      end
-    end
-
-    def insert_middleware
-      if defined? BetterErrors::Middleware
-        app.middleware.insert_after BetterErrors::Middleware, DevToolbar::Middleware
-      else
-        app.middleware.use DevToolbar::Middleware
-      end
-    end
-
-
-    def enable?
-      !Rails.env.production? and app.config.consider_all_requests_local
-    end
-
-    def app
-      Rails.application
-    end
   end
 end
