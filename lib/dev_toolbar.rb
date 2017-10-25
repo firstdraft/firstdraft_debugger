6a1ae2e4e1a3dfdfd867e0bcb3f2c465ebacc19a
Murugan <murugu2012@gmail.com>
2017-10-25 15:33:22 +0530

    Revert "Extract middleware to separate gem"
    This reverts commit 11e3ad90ac44f59b7928980a16506954e1c5dc61.

--- lib/dev_toolbar.rb
+++ lib/dev_toolbar.rb
@@ -1,4 +1,5 @@
 require "dev_toolbar/engine"
+require "dev_toolbar/middleware"
 require "dev_toolbar/debug_helper"
 module DevToolbar
   # Your code goes here...
---
+++ lib/dev_toolbar/middleware.rb
@@ -0,0 +1,37 @@
+module DevToolbar
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+      @ip = request.ip
+      @app.call(env)
+    rescue Exception => ex
+      if pass_through?
+        @app.call(env)
+      else
+        [200, {"Content-Type" => "text/html"}, [error_message]]
+      end
+    end
+
+    def pass_through?
+      path = Rails.root.join('whitelist.yml')
+      if File.exist?(path)
+        whitelisted_ips = YAML.load_file(path)
+        whitelisted_ips.include?(@ip)
+      else
+        true
+      end
+    end
+
+    def error_message
+  %Q{Uh oh! Something went wrong.
+
+ If you are the application owner, to get a more helpful error page please run this command at a terminal prompt:
+
+ `bin/whitelist #{@ip}`}
+    end
+  end
+end
